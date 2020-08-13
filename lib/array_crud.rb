dcd698eadc23194b748928d5430181feb5fe7cd0
Anthony Alcorn <xmastony@gmail.com>
2020-08-13 22:36:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   ["John", "Paul", "George", "Ringo"]
 end
 
-def add_element_to_end_of_array(create_an_array, Eric Clapton)
-  create_an_array.push ["Eric Clapton"]
+def add_element_to_end_of_array(array, element)
+  
 end
 
 def add_element_to_start_of_array(array, element)
